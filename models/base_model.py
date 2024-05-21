2a71f8dffd03ef712418f6f82298db21359fe22d
Mavenry <sundaybukola10@gmail.com>
2024-05-21 07:52:30 -0700

    basemodel updated

--- models/base_model.py
+++ models/base_model.py
@@ -3,11 +3,33 @@
 
 import uuid
 from datetime import datetime
-from models import storage
+from models import file_storage
 
 
 class BaseModel:
 
-        """Class from which all other classes will inherit"""
+    """All classes inherits from this class"""
+    def __init__(self):
 
-    pass
+        """attributes to be used by the instances"""
+
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now
+        self.updated_at = datetime.now
+    def __str__(self):
+
+        """returns a string representation"""
+
+        return f"[{__class__.__name__}] ({self.id}) {self.__dict__}"
+    def save(self):
+
+        """update the public instance"""
+
+        self.updated_at = datetime.now()
+    def to_dict(self):
+        """returns dictionary containing all keys and value pair"""
+        json_dict = self.__dict__()
+        json_dict["__class__"] = self.__class__.__name__
+        json_dict["created_at"] = json_dict["created_at"].isoformat()
+        json_dict["updated_at"] = json_dict["updated_at"].isoformat()
+        return json_dict
