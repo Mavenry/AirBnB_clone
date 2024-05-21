61de199329d6288c47ea2a1ece16b15a56e48199
Mavenry <sundaybukola10@gmail.com>
2024-05-21 06:06:43 -0700

    base model

---
+++ models/base_model.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""This script is the base model"""
+
+import uuid
+from datetime import datetime
+from models import storage
+
+
+class BaseModel:
+
+        """Class from which all other classes will inherit"""
+
+    pass
